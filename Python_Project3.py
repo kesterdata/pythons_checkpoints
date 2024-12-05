48ba9d256ba64ec49070c655e0968694a6c17aba
kesterdata <kesteranalytica@gmail.com>
2024-12-05 14:35:00 +0100

    Python Project checkpoint

--- Python_Project3.py
+++ Python_Project3.py
@@ -1,56 +1,74 @@
+from random import shuffle
 
-
-questions = {
+questions = [{
     """ 1. What is the name of the cat:
     A. jane
     B. Rainy
     C. Dane
-""" : "A",
+    """ : "A"},
 
-        """2. What is pip:
+    {"""2. What is pip:
     
-A. class
-B. package
-C. Library
+    A. class
+    B. package
+    C. Library
 
-""" : "B",
+    """ : "B"},
 
 
-"""        3. Which is these data structure:
-    
-A. class
-B. package
-C. Library
+    {"""3. Which is these data structure:
+        
+    A. class
+    B. package
+    C. Library
 
     """:"B"
 
-}
+}]
+#shuffle questions
+shuffle(questions)
+
 #score tracking
+
+highest_score =0
 
 should_continue = True
 
 def quiz():
-        total_score = 0
-        highest_score =0
-        #Display multiple-choice questions to the user.
-        for current_question, current_answer in questions.items():
-            print(current_question)
+    total_score = 0
+    global highest_score,should_continue
+   
+    #Display multiple-choice questions to the user.
+    for question_number, question in enumerate (questions, start=1):
+        current_question = list(question.keys())[0]
+        current_answer = list(question.values())[0]
+        print(current_question)
+
 
         #Capture user inputs.
-            user_response = input("Choose the correct answer : ").capitalize
-            print(user_response)
+        user_response = input("Choose the correct answer : ").capitalize()
+        print(user_response)
 
         #validate  user inputs.
         if user_response == current_answer:
             total_score += 1
 
-            #Provide immediate feedback on the correctness of answers
+        #Provide immediate feedback on the correctness of answers
         else:
             print(f"Your answer was wrong. The current answer is {current_answer} \n")
-        highest_score = max(total_score,highest_score)
+
+    highest_score = max(total_score,highest_score)
 
     #Display total score
-print(f"Your current score is {total_score} and your highest_score  is{highest_score}")
+    print(f"Your current score is : {total_score} and your highest_score  is : {highest_score}")
 
-choice = input("Do you want to try again?. Please type yes/no").lower()
-if choice == "yes"
+    choice = input("Do you want to try again?. Please type yes/no : ").lower()
+
+    if choice == "yes":
+        quiz()
+    else:
+        should_continue = False
+        print("Thanks for attempting")
+
+while should_continue:
+     quiz()
